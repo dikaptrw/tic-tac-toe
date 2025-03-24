07f648927fa10ed19ecc0c142a3c954b5fa778ab
dikaptrw <dikaptrw@gmail.com>
2025-03-24 22:24:02 +0800

    fix: move metadata declaration to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,4 @@
-import { Metadata } from "next";
 import GameContainer from "./components/GameContainer";
-import { getGlobalMetadata } from "./utils/metadata";
-
-export const metadata: Metadata = {
-  ...getGlobalMetadata(),
-};
 
 export default function Home() {
   return (
